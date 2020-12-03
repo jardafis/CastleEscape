7a8eff9f31c620d55829a5c61652c6fb2f8f4f6a
Craig Hackney <craig@digitalxfer.com>
2020-12-02 20:37:46 -0800

    Support only one compiler, sdcc.

--- main.c
+++ main.c
@@ -1,28 +1,21 @@
 #include <stdio.h>
 #include <input.h>
 #include <intrinsic.h>
-#ifndef __SDCC
-#include <spectrum.h>
-#else
 #include <im2.h>
 #include <string.h>
 #include <z80.h>
 #include <arch/zx.h>
-#endif
 #include "screentab.h"
-#include "brick.h"
+#include "tiled.h"
 
-#ifdef __SDCC
-extern void myputs(char *string);
-extern void myputc(char c) __banked ;
-#else
-extern void myputs(char *string)
-__banked;
-extern void myputc(char c)
-__banked;
-#endif
-
-extern void brick1(void);
+extern const unsigned char tile0[];
+extern void screen;
+extern unsigned char keyboardScan(void);
+extern void displayScreen(void *scr);
+extern void cls(char attr)
+__z88dk_fastcall;
+extern unsigned char updateDirection(void)
+__z88dk_fastcall;
 
 #define JUMP_POINT_BYTE        0x81
 #define TABLE_ADDR             ((void*) 0x8000)
@@ -30,19 +23,8 @@ extern void brick1(void);
 
 static int ticks = 0;
 
-#ifndef __SDCC
-int writebyte(int fd, int c)
-{
-
-}
-#endif
-
-#ifdef __SDCC
 void isr(void);
 IM2_DEFINE_ISR( isr)
-#else
-void isr(void)
-#endif
 {
     static unsigned char a = 0;
     zx_border(a++ & 0x7);
@@ -51,85 +33,62 @@ void isr(void)
 
 void initISR(void)
 {
-#ifdef __SDCC
     intrinsic_di();
     memset(TABLE_ADDR, JUMP_POINT_BYTE, 257);
     im2_init(TABLE_ADDR);
     z80_bpoke(JUMP_POINT, 0xc3);
     z80_wpoke(JUMP_POINT + 1, (unsigned int) isr);
     intrinsic_ei();
-#else
-    intrinsic_di();
-    zx_im2_init(TABLE_ADDR, 0x81);
-    im1_install_isr(isr);
-    intrinsic_ei();
-#endif
 }
 
-void brick(void)
+inline void drawTile(unsigned char tileID, unsigned char x, unsigned char y)
 {
-    for (int x = 0; x < 32; x++)
+    const unsigned char *tiles = &tile0[0];
+    if (tileID)
     {
-
-        for (char n = 0; n < 8; n++)
+        for (unsigned char n = 0; n < 8; n++)
         {
-            *(screenTab[(0 << 3) + n] + x) = MagickImage[7 + n];
-        }
-
-    }
-    for (int x = 0; x < 32; x++)
-    {
-        for (char n = 0; n < 8; n++)
-        {
-            *(screenTab[(23 << 3) + n] + x) = MagickImage[7 + n];
+            *(screenTab[(y << 3) + n] + x) = tiles[((tileID - 1) * 8) + n];
         }
     }
 }
 
-void func1(void) __banked;
+void brick(TILE_MAP *tileMap)
+{
+    for (unsigned char y = 0; y < 24; y++)
+    {
+        for (unsigned char x = 0; x < 32; x++)
+        {
+            drawTile(tileMap->data[(y * 32) + x], x, y);
+        }
+    }
+}
 
-extern int bankedtapeloader() __z88dk_fastcall;
 int main()
 {
-    int start, end;
     char key = 0;
-
-//    bankedtapeloader();
+    static unsigned char dir;
 
     initISR();
-#ifdef __SDCC
-    zx_cls(INK_WHITE | PAPER_BLACK);
-#else
-    zx_cls();
-    zx_colour(INK_WHITE | PAPER_BLACK);
-#endif
+    cls(INK_WHITE | PAPER_BLACK);
     createScreenTab();
 
-    printf("\n\n");
-    printf("Calling bank 1\n");
-    func1();
-    printf("Returned from bank 1\n");
-
     intrinsic_halt();
-    start = ticks;
-    brick();
-    end = ticks;
-    printf("%d\n", end - start);
+    displayScreen((void*) &screen);
 
-    intrinsic_halt();
-    start = ticks;
-    brick1();
-    end = ticks;
-    printf("%d\n", end - start);
-#ifdef __SDCC
-    while ((key = in_inkey()) == 0)
-#else
-    while ((key = in_Inkey()) == 0)
-#endif
+    while ((key = keyboardScan()) != '\n')
     {
         intrinsic_halt();
+
+        // Scan Q, A, O, P, SPACE and update the direction flags accordingly
+        dir = updateDirection();
+        if(dir)
+        {
+            printf("0x%02x ", dir);
+        }
     }
-    myputc(key);
+
+//    printf("%c\n", key);
     intrinsic_di();
     intrinsic_im_1();
     return (0);
