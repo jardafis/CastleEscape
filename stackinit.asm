6964726936977eaeefa6a892f82218add551f49c
Craig Hackney <iratahack@digitalxfer.com>
2020-12-14 14:56:21 -0800

    Updated comments.

--- stackinit.asm
+++ stackinit.asm
@@ -3,6 +3,9 @@
 		; Fill the stack with a known pattern so
 		; we can see how much we are using.
 		;
+		; Interrupts should be disabled so no need to worry
+		; about ISR accessing the stack.
+		;
 		; The top of the stack is 0x8181 and it is 0x80 bytes deep
 		defc	STACK_SIZE				= 0x80
 		defc	FILL_WORD				= 0x5555
@@ -12,5 +15,5 @@ fillStack:
 		ld		b,STACK_SIZE/2			; Stack size in words
 .fillStackLoop
 		push	de						; Push data to stack
-		djnz	fillStackLoop			; Loop to previous word
-		ld		sp,hl					; Restore stack address
+		djnz	fillStackLoop			; Loop for all words
+		ld		sp,hl					; Restore saved stack address
