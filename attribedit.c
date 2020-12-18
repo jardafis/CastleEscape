0f1315703b7299b5b65d232499d948c0dcddfeec
Craig Hackney <iratahack@digitalxfer.com>
2020-12-18 11:21:20 -0800

    Cleanup of tileset attribute editor.

--- attribedit.c
+++ attribedit.c
@@ -1,4 +1,3 @@
-#include <intrinsic.h>
 #include <arch/zx.h>
 
 extern unsigned char keyboardScan(void)
@@ -6,68 +5,125 @@ __z88dk_fastcall;
 extern void cls(char attr)
 __z88dk_fastcall;
 extern unsigned char *screenTab[];
-extern const unsigned char tile0[];
-extern const unsigned char tileAttr[];
 
-void displayTileset(void)
+/*
+ * Current cursor location for displaying text to the screen.
+ */
+static char cursorX = 0;
+static char cursorY = 0;
+
+/*
+ * Set the location for the next character printed to the screen.
+ */
+void setCursor(char x, char y)
 {
-    int x;
-    int y;
-    int n;
-    const unsigned char *tiles = &tile0[0];
-    unsigned char *screenAddr;
+    cursorX = x;
+    cursorY = y;
+}
+/*
+ * Address of the ROM font.
+ */
+const unsigned char *font = (const unsigned char*) 0x3d00;
+/*
+ * Start of the screen attribute memory.
+ */
+unsigned char *attr = (unsigned char*) 0x5800;
 
-    for (y = 0; y < 12; y++)
+/*
+ * Display a character using the ROM font at the specified x,y character
+ * screen location.
+ */
+void printChar(unsigned char c, unsigned char x, unsigned char y)
+{
+    for (char n = 0; n < 8; n++)
     {
-        for (x = 0; x < 12; x++)
+        *(screenTab[(y << 3) + n] + x) = font[((c - ' ') << 3) + n];
+    }
+}
+
+/*
+ * Output a string to the screen at the current x,y cursor position.
+ */
+void putString(unsigned char *string)
+{
+    while (*string)
+    {
+        if (*string == '\n')
         {
-            screenAddr = screenTab[y * 8] + x;
-            for (n = 0; n < 8; n++)
+            cursorX = 0;
+            cursorY++;
+        }
+        else
+        {
+            printChar(*string, cursorX++, cursorY);
+        }
+        string++;
+    }
+}
+
+/*
+ * Display the 12x12 tileset starting from the top left-hand side of the
+ * screen.
+ */
+void displayTileset(const unsigned char *tiles)
+{
+    for (char y = 0; y < 12; y++)
+    {
+        for (char x = 0; x < 12; x++)
+        {
+            unsigned char *screenAddr = screenTab[y * 8] + x;
+            for (char n = 0; n < 8; n++)
             {
                 *screenAddr = *tiles++;
                 screenAddr += 0x100;
             }
         }
     }
-
 }
 
+/*
+ * Bitmap for the box cursor.
+ */
 const unsigned char cursor[8] =
 { 0xff, 0x81, 0x81, 0x81, 0x81, 0x81, 0x81, 0xff };
 
+/*
+ * Display the box cursor at the current cursor location.
+ */
 void xorCursor(int x, int y)
 {
     unsigned char *screenAddr = screenTab[y * 8] + x;
-    int n;
 
-    for (n = 0; n < 8; n++)
+    for (char n = 0; n < 8; n++)
     {
         *screenAddr ^= cursor[n];
         screenAddr += 0x100;
     }
-
 }
 
-void setAttrib(int x, int y, unsigned char key)
+/*
+ * Set the ink color for the current cursor location.
+ */
+void setInk(int x, int y, unsigned char key)
 {
-    unsigned volatile char *attr = (unsigned char*) 0x5800;
-
     attr[(y * 32) + x] = key - '0';
 }
 
+/*
+ * Toggle the flash or bright attribute for the current cursor location.
+ */
 void toggleAttrib(int x, int y, unsigned char attrib)
 {
-    unsigned volatile char *attr = (unsigned char*) 0x5800;
-
     attr[(y * 32) + x] ^= attrib;
 }
 
-void saveAttrib(void)
+/*
+ * Save the attributes from the screen to the specified location.
+ */
+void saveAttrib(unsigned char *dest)
 {
     int x;
     int y;
-    unsigned volatile char *dest = (unsigned char*) 0xc000;
-    unsigned volatile char *attr = (unsigned char*) 0x5800;
 
     for (y = 0; y < 12; y++)
     {
@@ -78,91 +134,114 @@ void saveAttrib(void)
     }
 }
 
-void loadAttrib(void)
+/*
+ * Load attributes to screen memory from the specified location.
+ */
+void loadAttrib(unsigned char *src)
 {
     int x;
     int y;
-    unsigned char *dest = (unsigned char*) 0x5800;
-    const unsigned char *attr = &tileAttr[0];
 
     for (y = 0; y < 12; y++)
     {
         for (x = 0; x < 12; x++)
         {
-            dest[(y * 32) + x] = *attr++;
+            attr[(y * 32) + x] = *src++;
         }
     }
 }
 
-void attribEdit(void)
+/*
+ * Entry point for tile attribute editing in realtime.
+ */
+void attribEdit(unsigned char *tileset, unsigned char *attrib)
 {
     int x = 0;
     int y = 0;
     unsigned char key;
-    intrinsic_di();
 
     cls(INK_WHITE | PAPER_BLACK);
+    setCursor(0, 12);
+    putString("Up          - 'q'\n");
+    putString("Down        - 'a'\n");
+    putString("Left        - 'o'\n");
+    putString("Right       - 'p'\n");
+    putString("Bright      - 'b'\n");
+    putString("Flash       - 'f'\n");
+    putString("Reset       - 'r'\n");
+    putString("Ink color   - 0 - 7\n");
+    putString("Exit        - <SPACE>\n");
 
-    loadAttrib();
+    loadAttrib(attrib);
 
-    displayTileset();
+    displayTileset(tileset);
 
     xorCursor(x, y);
 
     do
     {
+        // Wait for key press
         while ((key = keyboardScan()) == 0)
-            ;
-        // Wait for key release
-        while (keyboardScan() != 0)
             ;
 
         switch (key)
         {
-        case 'P':
+        case 'P':                           // Right
             xorCursor(x, y);
             if (x < 11)
                 x++;
             xorCursor(x, y);
             break;
-        case 'O':
+        case 'O':                           // Left
             xorCursor(x, y);
             if (x > 0)
                 x--;
             xorCursor(x, y);
             break;
-        case 'Q':
+        case 'Q':                           // Up
             xorCursor(x, y);
             if (y > 0)
                 y--;
             xorCursor(x, y);
             break;
-        case 'A':
+        case 'A':                           // Down
             xorCursor(x, y);
             if (y < 11)
                 y++;
             xorCursor(x, y);
             break;
         default:
-            if (key >= '0' && key <= '7')
+            if (key >= '0' && key <= '7')   // Colors
             {
-                setAttrib(x, y, key);
+                setInk(x, y, key);
             }
-            else if (key == 'B')
+            else if (key == 'B')            // Bright
             {
-                toggleAttrib(x, y, 0x40);
+                toggleAttrib(x, y, BRIGHT);
             }
-            else if (key == 'F')
+            else if (key == 'F')            // Flash
             {
-                toggleAttrib(x, y, 0x80);
+                toggleAttrib(x, y, FLASH);
             }
-            else if (key == 'R')
+            else if (key == 'R')            // Reset
             {
-                loadAttrib();
+                loadAttrib(attrib);
             }
             break;
         }
 
-    } while (key != ' ');
-    saveAttrib();
+        // Wait for key release
+        while (keyboardScan() != 0)
+            ;
+    } while (key != ' ');                   // SPACE to exit
+    /*
+     * Copy the attributes to spare memory so they can be dumped.
+     */
+    saveAttrib((unsigned char*) 0xc000);
+
+    /*
+     * Copy the attributes back to their original location so
+     * they can be reflected in the game.
+     */
+    saveAttrib(attrib);
 }
