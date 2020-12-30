a0e6aad0d8d98d70297dc3651362ca8087a2153f
Craig Hackney <iratahack@digitalxfer.com>
2020-12-30 15:04:13 -0800

    Converted to run on CentOS 7.

--- assets/tiles.sh
+++ assets/tiles.sh
@@ -17,8 +17,9 @@ do
 		let tx=$x*8
 		let ty=$y*8
 		echo "$tx,$ty tile${tile}.png -> tile${tile}.h"
-		magick convert ${tileSheet} -crop 8x8+$tx+$ty tiles/tile${tile}.png
-		magick convert tiles/tile${tile}.png -threshold 50% -negate tiles/tile${tile}.h
+		convert ${tileSheet} -crop 8x8+$tx+$ty tiles/tile${tile}.png
+#		convert tiles/tile${tile}.png -threshold 50% -negate tiles/tile${tile}.h
+		convert tiles/tile${tile}.png -threshold 50% tiles/tile${tile}.h
 		sed -i s/MagickImage/tile${tile}/ tiles/tile${tile}.h
 		sed -i "s/0x50, 0x34, 0x0A, 0x38, 0x20, 0x38, 0x0A, //g" tiles/tile${tile}.h
 		sed -i "s/static //" tiles/tile${tile}.h
