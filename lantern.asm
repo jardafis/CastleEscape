29dbcddf17f1f8c252cd8a3ef1908d03b3b978ee
Craig Hackney <iratahack@digitalxfer.com>
2020-12-30 20:14:18 -0800

    Dynamically create the lantern list as the tilemap is processed.

--- lantern.asm
+++ lantern.asm
@@ -1,59 +1,53 @@
 		public	_lanternFlicker
+		extern	ticks
 		include	"defs.asm"
 
 		section	code_user
-
+		;
+		; On entry:
+		;			hl - Pointer to lantern table
+		;
 _lanternFlicker:
 		push	af
+
+		ld		a,(hl)					; Number of lanterns
+		or		a
+		jr		z,done					; No lanterns
+
 		push	bc
 		push	de
 		push	hl
 
-		ld		hl,lanterns				; Pointer to table of lanterns
-
-		ld		b,(hl)					; Number of lanterns
-		inc		hl
+		inc		hl						; Point to first attribute address
+		ld		b,a
 .loop
 		ld		e,(hl)					; Get low byte of address
 		inc		hl
 		ld		d,(hl)					; Get high byte of address
 		inc		hl
+		; DE is the pointer to the attribute memory
 
-		push	hl						; Save lantern pointer
-		; HL is the pointer to the attribute memory
+		ld		a,(ticks)
+		and		0x03					; Bottom 2 bits are the index into color table
 
-		ld		a,(toggle)
-		inc		a
-		ld		(toggle),a
-		and		0x03					; Bottom 2 bits are index into color table
+		push	bc						; Save loop counter
+		ld		bc,colors				; Pointer to color table
+		add		c						; 'colors' address is aligned by 4 so add it to 'a'
+		ld		c,a						; 'hl' now points to our color attribute
 
-		ld		hl,colors				; Pointer to color table
-		add		a,l						; 'colors' address is aligned by 4 so add it to 'a'
-		ld		l,a						; 'hl' now points to our color attribute
-
-		ld		a,(hl)					; Read attribute
+		ld		a,(bc)					; Read attribute
 		ld		(de),a					; and write it to the screen
 
-		pop		hl						; Restore lantern pointer
+		pop		bc						; Restore loop counter
 		djnz	loop
 
 		pop		hl
 		pop		de
 		pop		bc
+.done
 		pop		af
 		ret
 
-		section bss_user
-.toggle
-		db		0
-
 		section rodata_user
-		ds		$ - ($ & 0x03) + 0x03, 0x55	; Align by 4
 .colors
-		db		INK_YELLOW | BRIGHT, INK_YELLOW, INK_RED, INK_RED | BRIGHT
-
-.lanterns	;   y,x
-		db		3						; Num lanterns
-		dw		SCREEN_ATTR_START + (16 * SCREEN_WIDTH) + 15
-		dw		SCREEN_ATTR_START + (8 * SCREEN_WIDTH) + 21
-		dw		SCREEN_ATTR_START + (16 * SCREEN_WIDTH) + 28
+		db		(INK_YELLOW | BRIGHT), INK_YELLOW, INK_RED, (INK_RED | BRIGHT)
