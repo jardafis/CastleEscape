2bd0ba0a1a7147f087dc375348c679534499f6d4
Craig Hackney <craig@digitalxfer.com>
2021-01-06 22:03:48 -0800

    Lowered the color threshold to 1% so the sprite sheet can have colors.

--- assets/tiles.sh
+++ assets/tiles.sh
@@ -19,7 +19,7 @@ do
 		echo "$tx,$ty tile${tile}.png -> tile${tile}.h"
 		convert ${tileSheet} -crop 8x8+$tx+$ty tiles/tile${tile}.png
 #		convert tiles/tile${tile}.png -threshold 50% -negate tiles/tile${tile}.h
-		convert tiles/tile${tile}.png -threshold 50% tiles/tile${tile}.h
+		convert tiles/tile${tile}.png -threshold 1% tiles/tile${tile}.h
 		sed -i s/MagickImage/tile${tile}/ tiles/tile${tile}.h
 		sed -i "s/0x50, 0x34, 0x0A, 0x38, 0x20, 0x38, 0x0A, //g" tiles/tile${tile}.h
 		sed -i "s/static //" tiles/tile${tile}.h
