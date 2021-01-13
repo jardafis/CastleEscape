cfd3fba42232fb5e611f857cc4cb76ff1dcc3f6f
Craig Hackney <iratahack@digitalxfer.com>
2021-01-12 23:08:11 -0800

    Initial.

---
+++ sprite/RightSprite.c
@@ -0,0 +1,9 @@
+/* Auto-generated sprite */
+#include "RightSprite0.h"
+#include "RightSprite1.h"
+#include "RightSprite2.h"
+#include "RightSprite3.h"
+#include "RightSprite4.h"
+#include "RightSprite5.h"
+#include "RightSprite6.h"
+#include "RightSprite7.h"
