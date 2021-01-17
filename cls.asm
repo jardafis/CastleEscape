aa72ccd12472080ae5f52fbaf7ad72c1f175199e
Craig Hackney <craig@digitalxfer.com>
2021-01-16 21:48:01 -0800

    Added routines to clear an attribute location and a character location.

--- cls.asm
+++ cls.asm
@@ -1,4 +1,7 @@
+        extern	_screenTab
         public  _cls
+		public	clearAttr
+		public	clearChar
         section code_user
 
         include "defs.asm"
@@ -9,8 +12,12 @@
         ; On entry, l contains the attribute to fill the attr memory.
         ;
 _cls:
+        push	af
         push    bc
         push    hl
+
+		ld		a,l
+		ld		(clsAttrib),a
 
         halt    
 
@@ -59,4 +66,69 @@ _cls:
 
         pop     hl
         pop     bc
+        pop		af
         ret     
+
+		;
+		; Set the screen attribute position specified by 'bc' to
+		; the attribute used to clear the screen.
+		;
+		; Entry:
+		;		b - Attrib Y location
+		;		c - Attrib X location
+		;
+		; Corrupts:
+		;		af, bc, hl
+		;
+clearAttr:
+		ld		l,b
+		ld		h,0
+		hlx		32
+		ld		b,0
+		add		hl,bc
+		ld		bc,SCREEN_ATTR_START
+		add		hl,bc
+		ld		a,(clsAttrib)
+		ld		(hl),a
+		ret
+
+		;
+		; Clear the character position specified by 'bc'
+		;
+		; Entry:
+		;		b - Char Y location
+		;		c - Char X location
+		;
+		; Corrupts:
+		;		af, bc, hl
+		;
+clearChar:
+		ld		l,b
+		ld		h,0
+		hlx		16
+
+		di
+		ld		(clearCharSP),sp
+		ld		sp,_screenTab
+		add		hl,sp
+		ld		sp,hl
+		pop		hl
+		ld		a,l
+		add		c
+		ld		l,a
+		xor		a
+		ld		b,8
+.clearCharLoop
+		ld		(hl),a
+		inc		h
+		djnz	clearCharLoop
+
+.clearCharSP = $ + 1
+		ld		sp,0x0000
+		ei
+
+		ret
+
+		section bss_user
+clsAttrib:
+		ds		1
