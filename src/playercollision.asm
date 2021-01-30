d48c25edcf31857f82a6c1e6b458a83ef417be71
Craig Hackney <craig@digitalxfer.com>
2021-01-29 21:36:28 -0800

    Optimized collision routines.

--- src/playercollision.asm
+++ src/playercollision.asm
@@ -19,62 +19,40 @@
         defc    ID_SOFT_TILE=139
 
         ;
-        ; Check for player colliding with solid platforms on
+        ; Check for player colliding with platforms on
         ; the X axis and if the player is going off the screen
         ; to the left or right display the next or previous
-        ; screen.
+        ; level.
         ;
         ; Entry:
         ;		a - xSpeed
         ;
+        ; Register variables:
+        ;		b - xSpeed
+        ;		c - yPos
+        ;
 checkXCol:
-        ld      b, a                    ; Save xSpeed
-IF  1
-        ld      hl, (_xPos)             ; Get the X pixel offset
-        or      a                       ; Update flags
-        jp      p, pos2                 ; If positive
-        dec     hl                      ; else negative, subtract 1 from xPos
-        jr      endif
-pos2:
-        ld      de, PLAYER_WIDTH        ; else add player width
-        add     hl, de
-endif:
-        ex      de, hl
-
-        ld      hl, (_yPos)             ; Get the yPos it has already been updated by checkYCol
-        ld      a, -24                  ; Subtract the delta between the screen offset and the level offset
-        add     l                       ; Add the current y position
-        ld      c, a                    ; save it in 'c'
-        and     %11111000               ; Remove the pixel offset within the byte (lower 3 bits)
-        ld      l, a
-        hlx     TILEMAP_WIDTH/8         ; Divide by 8 to get byte offset and multiply by 128 (width of tilemap)
-ELSE    
-        ld      hl, (_yPos)             ; Get the yPos it has already been updated by checkYCol
-        ld      a, -24                  ; Subtract the delta between the screen offset and the level offset
-        add     l                       ; Add the current y position
-        ld      c, a                    ; save it in 'c'
-        and     %11111000               ; Remove the pixel offset within the byte (lower 3 bits)
-        ld      l, a
-        hlx     TILEMAP_WIDTH/8         ; Divide by 8 to get byte offset and multiply by 128 (width of tilemap)
-
         ld      de, (_xPos)             ; Get the X pixel offset
-        ld      a, b                    ; speed may be positive or negative
-        or      a                       ; Update flags
-        jp      p, pos2                 ; If positive
-        dec     d                       ; else negative, subtract 1 from hi-order byte
-pos2:
-        addde                           ; Add 'a'
-
-        ld      a, b                    ; Get speed again
-        or      a                       ; Update flags
-        jp      m, neg1                 ; If negative
-        ld      a, PLAYER_WIDTH-1       ; else add player width
+        ld      b, a                    ; Save xSpeed
+;        or      a                       ; Flags were updated based on this value before we were called
+        jp      p, movingRight          ; If positive moving right
+        dec     de                      ; else negative, moving left. Subtract 1 from xPos.
+        jr      endif
+movingRight:
+        ld      a, PLAYER_WIDTH         ; else add player width
         addde   
-neg1:
-ENDIF   
-        ; Divide by 8 to get byte offset
+endif:
+        ld      hl, (_yPos)             ; Get the yPos
+        ld      a, -24                  ; Subtract the delta between the screen offset and the level offset
+        add     l                       ; Add the current y position
+        ld      c, a                    ; save it in 'c'
+        and     %11111000               ; Remove the pixel offset within the byte (lower 3 bits)
+        ld      l, a
+        hlx     TILEMAP_WIDTH/8         ; Divide by 8 to get byte offset and multiply by width of tilemap
+
+        ; Divide xPos by 8 to get byte offset
         ld      a, e
-        sra     d                       ; SRA leaves the sign bit (bit-7) intact. Good for signed shifts.
+        sra     d
         rra     
         sra     d
         rra     
@@ -85,6 +63,7 @@ ENDIF
 
         ld      de, (_currentTileMap)
         add     hl, de
+
         ld      a, (hl)
         cp      ID_SOLID_TILE
         ret     nc                      ; 'nc' if a >= value
@@ -92,13 +71,14 @@ ENDIF
         ; Check the bottom half of the sprite
         ld      de, TILEMAP_WIDTH
         add     hl, de
+
         ld      a, (hl)
         cp      ID_SOLID_TILE
         ret     nc                      ; 'nc' if a >= value
 
-        ld      a, c                    ; Restore yPos + ySpeed
+        ld      a, c                    ; Restore yPos
         and     %00000111               ; If the lower 3 bits are zero player has not shifted into
-        jr      z, checkXDone           ; the next row down, return.
+        jr      z, checkXDone           ; the next row down, we are done.
         add     hl, de                  ; Next row down
 
         ld      a, (hl)
@@ -108,7 +88,7 @@ ENDIF
 checkXDone:
         ld      a, (_xPos)              ; Get the X pixel offset
         add     b                       ; Add speed
-        cp      0xff                    ; If new xPos is negative
+        cp      0xff                    ; If new xPos is -1
         jr      z, previousXLevel       ; display previous level.
         cp      MAX_X_POS-PLAYER_WIDTH+1
         jr      nc, nextXLevel          ; 'nc' if a >= value
@@ -119,45 +99,50 @@ previousXLevel:
         ld      a, (_tileMapX)
         or      a
         ret     z
+
         dec     a
-        ld      (_tileMapX), a
-        ld      a, MAX_X_POS-PLAYER_WIDTH
+        ld      b, MAX_X_POS-PLAYER_WIDTH
+                                        ; New x position
         jr      changeXLevel
 nextXLevel:
         ld      a, (_tileMapX)
         cp      MAX_LEVEL_X-1
         ret     z
+
         inc     a
-        ld      (_tileMapX), a
-        xor     a
+        ld      b, 0                    ; New x position
 changeXLevel:
+        ld      (_tileMapX), a
+        ld      a, b                    ; New x position
         ld      (_xPos), a
         call    _setupScreen
         ret     
 
         ;
-        ; Check for player colliding with solid platforms on
+        ; Check for player colliding with platforms on
         ; the Y axis and if the player is going off the screen
         ; to the top or bottom display the next or previous
-        ; screen.
+        ; level.
         ;
         ; Entry:
+        ;		None
         ;
+        ; Register variables:
+        ;		b - ySpeed
+        ;		c - xPos
         ;
 checkYCol:
-IF  1
-  IF    1
         ld      hl, (_yPos)
         ld      a, (_ySpeed)            ; Check if ySpeed is negative
         ld      b, a
         or      a
-        jp      p, pos3                 ; Moving down or stopped
+        jp      p, movingDown           ; Moving down or stopped
         ld      de, -25                 ; Pixel position above the player
-        jr      skip
-pos3:
+        jr      endif2
+movingDown:
         ld      b, 1                    ; Force gravity as ySpeed could by 0
         ld      de, PLAYER_HEIGHT-24    ; Pixel position below the player
-skip:
+endif2:
         add     hl, de
 
 		;
@@ -177,23 +162,6 @@ skip:
 		;
         hlx     TILEMAP_WIDTH
 
-  ELSE  
-        ld      a, (_ySpeed)            ; Check if ySpeed is negative
-        ld      b, a
-        or      a
-        ld      a, (_yPos)
-        jp      m, neg2                 ; If ySpeed is negative skip adding
-        ld      b, 1                    ; gravity
-        add     PLAYER_HEIGHT-1         ; and player height.
-neg2:
-        add     b                       ; Add y speed should be 1, or -1
-        add     -24                     ; Subtract the delta between the screen and the tilemap offset
-
-        and     %11111000               ; Remove the pixel offset within the byte (lower 3 bits)
-        ld      l, a
-        ld      h, 0
-        hlx     TILEMAP_WIDTH/8         ; Divide by 8 to get byte offset and multiply by width of tilemap
-  ENDIF 
         ld      a, (_xPos)              ; Get the X pixel offset
         ld      c, a                    ; Save pixel offset for later
         rrca                            ; Divide by 8 to get the byte offset
@@ -240,12 +208,13 @@ noYCollision:
         ; Transition to falling.
         ;  Clear X movement.
         ;  set ySpeed to 1 (down).
-        ;  set the falling flag.
+        ;  increment the falling counter.
         xor     a
         ld      (_xSpeed), a
         inc     a
         ld      (_ySpeed), a
-        ld      (_falling), a
+        ld      hl, _falling
+        inc     (hl)
 
 updateYPos:
 		;
@@ -309,177 +278,3 @@ landed:
         ld      (_jumping), a
         ld      (_falling), a
         ret     
-
-ELSE    
-        ld      a, (_ySpeed)            ; If jumping up ySpeed is negative,
-        or      a                       ; there is no gravity,
-        jp      m, moveUp               ; move up.
-
-        ld      a, (_yPos)
-        add     PLAYER_HEIGHT-24        ; Subtract the delta between the screen offset and the level offset
-        and     %11111000               ; Remove the pixel offset within the byte (lower 3 bits)
-        ld      l, a
-        ld      h, 0
-        hlx     TILEMAP_WIDTH/8         ; Divide by 8 to get byte offset and multiply by width of tilemap
-
-        ld      a, (_xPos)              ; Get the X pixel offset
-        ld      b, a                    ; Save pixel offset for later
-        rrca                            ; Divide by 8 to get the byte offset
-        rrca                            ; Faster to do rrca followed by AND rather than srl
-        rrca    
-        and     %00011111
-        addhl                           ; Add X byte offset to tile map Y index
-
-        ld      de, (_currentTileMap)
-        add     hl, de
-
-        ld      a, (hl)                 ; Get tile ID
-        cp      ID_SOFT_TILE
-        jr      nc, landed              ; 'nc' if a >= value
-
-        inc     hl                      ; Next tile to the right
-        ld      a, (hl)                 ; Get tile ID
-        cp      ID_SOFT_TILE
-        jr      nc, landed              ; 'nc' if a >= value
-
-        ld      a, b                    ; Restore X pixel offset
-        and     %00000111               ; Check if any of the lower 3 bits are set
-        jr      z, gravity              ; if not we are done
-        inc     hl                      ; Check the tile to the right
-        ld      a, (hl)
-        cp      ID_SOFT_TILE
-        jr      c, gravity              ; 'c' if a < value
-
-landed:
-        ;
-        ; Reset ySpeed and jumping count and falling flag
-        ;
-        xor     a
-        ld      (_ySpeed), a
-        ld      (_jumping), a
-        ld      (_falling), a
-        ret     
-
-gravity:
-        ld      a, (_jumping)           ; Check if player is in
-        or      a                       ; downward jump
-        jr      nz, moveDown
-
-        ;
-        ; Transition to falling.
-        ;  Clear X movement.
-        ;  set ySpeed to 1 (down).
-        ;  set the falling flag.
-        xor     a
-        ld      (_xSpeed), a
-        inc     a
-        ld      (_ySpeed), a
-        ld      (_falling), a
-moveDown:
-        ld      a, (_yPos)
-        inc     a
-        cp      MAX_Y_POS-PLAYER_HEIGHT-1
-        jr      nc, nextYLevel
-        ld      (_yPos), a
-        ret     
-
-moveUp:
-        ld      a, (_yPos)
-        dec     a
-        cp      24
-        jr      c, previousYLevel       ; 'c' if 'a' < 24
-
-        add     -24                     ; Subtract the delta between the screen offset and the level offset
-        and     %11111000               ; Remove the pixel offset within the byte (lower 3 bits)
-        ld      l, a
-        ld      h, 0
-        hlx     TILEMAP_WIDTH/8         ; Divide by 8 to get byte offset and multiply by width of tilemap
-
-        ld      a, (_xPos)              ; Get the X pixel offset
-        ld      b, a                    ; Save pixel offset for later
-        rrca                            ; Divide by 8 to get the byte offset
-        rrca                            ; Faster to do rrca followed by AND rather than srl
-        rrca    
-        and     %00011111
-        addhl                           ; Add X byte offset to tile map Y index
-
-        ld      de, (_currentTileMap)
-        add     hl, de
-
-        ld      a, (hl)                 ; Get tile ID
-        cp      ID_SOLID_TILE
-        ret     nc                      ; 'nc' if a >= value
-
-        inc     hl                      ; Next tile to the right
-        ld      a, (hl)                 ; Get tile ID
-        cp      ID_SOLID_TILE
-        ret     nc                      ; 'nc' if a >= value
-
-        ld      a, b                    ; Restore X pixel offset
-        and     %00000111               ; Check if any of the lower 3 bits are set
-        jr      z, up                   ; if not we are done
-        inc     hl                      ; Check the tile to the right
-        ld      a, (hl)
-        cp      ID_SOLID_TILE
-        ret     nc                      ; 'nc' if a >= value
-
-up:
-        ld      a, (_yPos)
-        dec     a
-        ld      (_yPos), a
-        ret     
-
-previousYLevel:
-        ld      a, (_tileMapY)
-        or      a
-        ret     z
-        ;
-        ; Check for a gap in the tiles above
-        ;
-        ld      de, (_currentTileMap)
-        ld      hl, -TILEMAP_WIDTH      ; Get previous tile row
-        add     hl, de
-
-        ld      a, (_xPos)              ; Get the X pixel offset
-        ld      b, a                    ; Save pixel offset for later
-        rrca                            ; Divide by 8 to get the byte offset
-        rrca                            ; Faster to do rrca followed by AND rather than srl
-        rrca    
-        and     %00011111
-        addhl                           ; Add X byte offset to tile map Y index
-
-        ld      a, (hl)                 ; Get tile ID
-        cp      ID_SOLID_TILE
-        ret     nc                      ; 'nc' if a >= value
-
-        inc     hl
-        ld      a, (hl)                 ; Get tile ID
-        cp      ID_SOLID_TILE
-        ret     nc                      ; 'nc' if a >= value
-
-        ld      a, b                    ; Restore X pixel offset
-        and     %00000111               ; Check if any of the lower 3 bits are set
-        jr      z, prev                 ; if not we are done
-        inc     hl                      ; Check the tile to the right
-        ld      a, (hl)
-        cp      ID_SOLID_TILE
-        ret     nc                      ; 'nc' if a >= value
-prev:
-        ld      a, (_tileMapY)
-        dec     a
-        ld      (_tileMapY), a
-        ld      a, MAX_Y_POS-PLAYER_HEIGHT
-        jr      changeYLevel
-nextYLevel:
-        ld      a, (_tileMapY)
-        cp      MAX_LEVEL_Y-1
-        ret     z
-        inc     a
-        ld      (_tileMapY), a
-        ld      a, 24
-changeYLevel:
-        ld      (_yPos), a
-        call    _setupScreen
-        ret     
-ENDIF   
-
