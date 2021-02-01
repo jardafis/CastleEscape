82b859c9508ed5bbee859fb4f4bba75ae7f47119
Craig Hackney <iratahack@digitalxfer.com>
2021-02-01 11:45:18 -0800

    Added sounds for falling and dying.

--- src/ayfxplay.asm
+++ src/ayfxplay.asm
@@ -205,11 +205,11 @@ afxNseMix:
         ; ------------------------------------------------- -------------;
 
 AFXPLAY:
-		push	af
-		push	bc
-		push	de
-		push	hl
-		push	ix
+        push    af
+        push    bc
+        push    de
+        push    hl
+        push    ix
 
         ld      de, 0                   ; in DE, longest search time
         ld      h, e
@@ -251,9 +251,9 @@ afxPlay1:
         ld      (ix-1), b               ; set the sound time to zero
         ld      (ix-0), b
 
-		pop		ix
-		pop		hl
-		pop		de
-		pop		bc
-		pop		af
+        pop     ix
+        pop     hl
+        pop     de
+        pop     bc
+        pop     af
         ret     
