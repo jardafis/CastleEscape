7f42544c9a38b8eb52cb63c205ad51c542b10974
Craig Hackney <craig@digitalxfer.com>
2021-02-10 12:58:18 -0800

    Small change in the way SP is saved and restored.

--- src/stackinit.asm
+++ src/stackinit.asm
@@ -10,10 +10,11 @@
         defc    STACK_SIZE=0x80
         defc    FILL_WORD=0x5555
 fillStack:
-        ld      hl, sp                  ; Save the current stack address
+        ld      (saveSP+1), sp          ; Save the current stack address
         ld      de, FILL_WORD           ; Word to fill
         ld      b, STACK_SIZE/2         ; Stack size in words
 fillStackLoop:
         push    de                      ; Push data to stack
         djnz    fillStackLoop           ; Loop for all words
-        ld      sp, hl                  ; Restore saved stack address
+saveSP:
+        ld      sp, -1                  ; Restore saved stack address
