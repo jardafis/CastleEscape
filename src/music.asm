ded19450da0f549b71e42c54d048756447aea297
Craig Hackney <iratahack@digitalxfer.com>
2021-02-11 16:14:23 -0800

    Added music to title and main menu.

---
+++ src/music.asm
@@ -0,0 +1,33 @@
+        public  PLAYER_INIT
+        public  PLAYER_OFF
+        public  LOAD_SONG
+        public  START_SONG
+
+        section BANK_5
+
+		;
+		; Assembly player from https://github.com/AugustoRuiz/WYZTracker
+		;
+		; Minor tweaks to change some routine names to English
+		;
+        include "wyzproplay47c_zx.inc"
+
+		;
+		; Table of songs...
+		;
+TABLA_SONG:
+        dw      SONG_0, SONG_1
+
+        ;
+        ; Instrument configuration exported from WYZTracker
+        ;
+        include "music/CastleEscape.mus.inc"
+
+		;
+		; Songs exported from WYZTracker
+		;
+SONG_0:
+        binary  "music/gothic.mus"
+SONG_1:
+        binary  "music/jinj_med.mus"
+
