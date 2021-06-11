57a6ac8e62ccc860103c5d6b11a747812396e0e0
Craig Hackney <iratahack@digitalxfer.com>
2021-06-10 18:30:03 -0700

    Added comments to show how banks are arranged.

--- src/bankedcall.asm
+++ src/bankedcall.asm
@@ -5,6 +5,27 @@
 
         include "defs.inc"
 
+		; 0xffff +--------+--------+--------+--------+--------+--------+--------+--------+
+		;        | Bank 0 | Bank 1 | Bank 2 | Bank 3 | Bank 4 | Bank 5 | Bank 6 | Bank 7 |
+		;        |        |        |(also at|        |        |(also at|        |        |
+		;        |        |        | 0x8000)|        |        | 0x4000)|        |        |
+		;        |        |        |        |        |        | screen |        | screen |
+		; 0xc000 +--------+--------+--------+--------+--------+--------+--------+--------+
+		;        | Bank 2 |        Any one of these pages may be switched in.
+		;        |        |
+		;        |        |
+		;        |        |
+		; 0x8000 +--------+
+		;        | Bank 5 |
+		;        |        |
+		;        |        |
+		;        | screen |
+		; 0x4000 +--------+--------+
+		;        | ROM 0  | ROM 1  | Either ROM may be switched in.
+		;        |        |        |
+		;        |        |        |
+		;        |        |        |
+		; 0x0000 +--------+--------+
 		;
 		; Note:
 		;		Alternate register set is used for temporary
