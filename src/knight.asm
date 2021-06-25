28eed8f646ae60ee782beab444b604d04c35799a
Craig Hackney <iratahack@digitalxfer.com>
2021-06-25 08:22:41 -0700

    Seperated knight sprites from tilesheet.

---
+++ src/knight.asm
@@ -0,0 +1,10 @@
+        public  _RightKnight0
+        public  _LeftKnight0
+
+        section RODATA_2
+
+_RightKnight0:
+        include "sprite/RightKnight.inc"
+_LeftKnight0:
+        include "sprite/LeftKnight.inc"
+
