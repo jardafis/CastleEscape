dfd5fc119120a4a7dee7742e2bb8cdfa2696ae4a
Craig Hackney <iratahack@digitalxfer.com>
2021-10-07 14:14:51 -0700

    Formatted shell script.

--- src/mkdep.sh
+++ src/mkdep.sh
@@ -3,20 +3,18 @@
 # Script to generate dependancy files for make
 #
 
-if [ $# -ne 1 ]
-then
+if [ $# -ne 1 ]; then
 	echo "Usage: $0 <source file>" >&2
 	exit 1
 fi
 
-if [ ! -e $1 ]
-then
+if [ ! -e $1 ]; then
 	echo "$1: file does not exist." >&2
 	exit 1
 fi
 
 file=$1
-INCS=`grep -iE "^\s*(include|binary)\s*\".*\"" $file | sed 's/\"//g' | awk '{print $2}'`
+INCS=$(grep -iE "^\s*(include|binary)\s*\".*\"" $file | sed 's/\"//g' | awk '{print $2}')
 echo "${file/.*/.o} ${file/.*/.d}: $file ${INCS//$'\n'/ }"
 
 exit 0
