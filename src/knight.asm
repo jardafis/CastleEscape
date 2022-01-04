4a6332cbe01e35c347f000d8465a56afd5b1d067
Craig Hackney <iratahack@digitalxfer.com>
2022-01-04 15:29:56 -0800

    Added sprite palette and pattern data to bank 4 for ZXN

--- src/knight.asm
+++ src/knight.asm
@@ -13,4 +13,17 @@ RightJumpKnight0:
         #include    "sprite/RightJumpKnight.inc"
 LeftJumpKnight0:
         #include    "sprite/LeftJumpKnight.inc"
+IF  _ZXN
+        section RODATA_4
 
+        public  spriteStart
+        public  spriteEnd
+        public  spritePalette
+        public  spritePaletteEnd
+spriteStart:
+        #include    "sprite/sprites.inc"
+spriteEnd:
+spritePalette:
+        db      0x00, 0x02, 0xa0, 0xa2, 0x14, 0x16, 0xb4, 0xb6, 0x00, 0x03, 0xe0, 0xe7, 0x1c, 0x1f, 0xfc, 0xff
+spritePaletteEnd:
+ENDIF
